2e95d72d0d3b9fbd271e00458ba826e7b3e6fb9b
chenkang404 <763711755@qq.com>
2023-09-27 10:24:39 +0800

    the 3rd commit

--- first_code.py
+++ first_code.py
@@ -1,6 +1,15 @@
+'''
+Author: chenkang404 763711755@qq.com
+Date: 2023-09-27 09:59:28
+LastEditors: chenkang404 763711755@qq.com
+LastEditTime: 2023-09-27 10:21:54
+FilePath: \codeBase\tools_samrt\first_code.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import os
 
 
 if __name__ == "__main__":
     print(os.getcwd())
     print("the 2ed commit")
+    print("the 3rd commit")
